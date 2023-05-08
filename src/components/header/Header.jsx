8eb81666f8c324c78186d16552940826bbdf3a7e
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-08 19:02:40 +0530

    first commit header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Harmeet Singh Gorowada</h1>
+        <h5 className='text-light'>Full Stack Developer</h5>
+      </div>
+    </header>
   )
 }
 

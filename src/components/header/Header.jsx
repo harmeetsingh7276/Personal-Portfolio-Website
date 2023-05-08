97d403a0a5576efb56e568ab14a1ada44ecd994a
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-08 19:31:29 +0530

    added profile picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import IMG from '../../assests/me.png'
 
 const Header = () => {
   return (
@@ -8,6 +10,13 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Harmeet Singh Gorowada</h1>
         <h5 className='text-light'>Full Stack Developer</h5>
+        <CTA/>
+
+        <div className="me">
+          <img src={IMG} alt="Me" srcset="" />
+        </div>
+
+        <a href="#contact" className='scroll_down'>Scroll Down</a>
       </div>
     </header>
   )

c5c088035a012fdde38b40248d804847f1947d4c
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-08 15:16:05 +0530

    added the remiaining components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Header from './components/header/Header'
+import About from './components/about/About'
+import Nav from './components/nav/Nav'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/porfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 export default function App() {
   return (
     <>
       <Header/>
+      <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </>
   )
 }

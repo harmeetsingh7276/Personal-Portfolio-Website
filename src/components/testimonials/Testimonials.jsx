d54c78f23f96479ccfec25cbe9adaf3a84a6f281
Harmeet Singh Gorowada <harmeetsingh7276@gmail.com>
2023-05-19 23:49:26 +0530

    swiper implementation

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,30 +5,33 @@ import AVTR2 from "../../assests/avatar2.jpg";
 import AVTR3 from "../../assests/avatar3.jpg";
 import AVTR4 from "../../assests/avatar4.jpg";
 
+// import Swiper core and required modules
+import { Pagination , Navigation} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 const data = [
   {
     avatar: AVTR1,
     name: "Mudassir Ali",
-    review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
+    review:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
   },
   {
     avatar: AVTR2,
     name: "Gayatri Shinde",
-    review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
+    review:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
   },
   {
     avatar: AVTR3,
     name: "Neeraj Baipureddy",
-    review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
+    review:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
   },
   {
     avatar: AVTR4,
     name: "Ashween Ramakrishan",
-    review:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
+    review:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde praesentium, quaerat at repellendus fugiat aperiam laboriosam, error voluptatem placeat vero itaque quo asperiores! Magni eius numquam velit commodi quos ducimus?",
   },
 ];
 const Testimonials = () => {
@@ -37,19 +40,26 @@ const Testimonials = () => {
       <h5>Review from Collegues</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials_container">
+      <Swiper className="container testimonials_container"
+       // install Swiper modules
+       modules={[ Pagination ]}
+       spaceBetween={40}
+       slidesPerView={1}
+       navigation
+       pagination={{ clickable: true }}
+      >
         {data.map(({ avatar, name, review }, index) => {
           return (
-            <article key={index} className="testimonial">
+            <SwiperSlide key={index} className="testimonial">
               <div className="client_avatar">
                 <img src={avatar} />
               </div>
               <h5 className="client_name">{name}</h5>
               <small className="client_review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
